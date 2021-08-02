b912d91fead22139e8ddcc8286110384d5790d98
dabagirevalens <vdabagire@gmail.com>
2021-08-02 22:37:37 +0200

    hanlering mongoose validation and mngoose wrong object id errors

--- middlewares/errors.js
+++ middlewares/errors.js
@@ -6,6 +6,22 @@ export default (err, req, res, next) => {
 
     error.message = err.message;
 
+    //Wrong Mongoose ObjectId Error
+
+    if(err.name === 'CastError'){
+        const message = `Resource not found. invalid : ${err.path}`;
+
+        error = new Error(message, 400);
+    }
+
+    //Handling mongoose validation errors
+
+    if(err.name === 'ValidationError') {
+        const message = Object.values(err.errors).map(value => value.message);
+
+        error = new Error(message, 400);
+    }
+
     res.status(err.statusCode).json({
         success: false,
         error,
