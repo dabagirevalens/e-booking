46ece96a3a1b143c67ee97537d90de370c72dacb
dabagirevalens <vdabagire@gmail.com>
2021-08-03 10:29:07 +0200

    create app store

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="py-1">
       <p className="text-center mt-1">
-        &copy;2021 Booking - Valens DABAGIRE || All Rights Reserved
+        &copy;2021 Booking - Valens DABAGIRE || All Rights Reserved.
       </p>
     </footer>
   );
--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -4,7 +4,7 @@ import Head from 'next/head';
 import Header from './Header';
 import Footer from './Footer';
 
-const Layout = ({ children, title = "BOOK BEST HOTELS FOR YOUR HOLIDAYS."}) => {
+const Layout = ({ children, title = "BOOK BEST HOTELS."}) => {
     return (
         <div>
             <Head>
