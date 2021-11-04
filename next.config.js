00f6e4e9762b80242fdf93283c19cbb1548fd72c
DABAGIRE Valens <83093092+dabagirevalens@users.noreply.github.com>
2021-11-04 21:23:54 +0200

    load stripe

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports = {
     DB_LOCAL_URL: "mongodb://localhost:27017/e-booking",
 
     DB_URI: 'mongodb+srv://dabagire:2ussy9luzZy9Jq31@e-booking.uk9ed.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-    
+
     STRIPE_API_KEY: 'pk_test_51JFOUzJt8PjGxb7vBnax4XXGVaMf9G78be5UMwj6EotdLe6mOOI6931bptYCxh0hh8ui6fsRHEXHU9u2nMNRiWEQ00GNPHuJFn',
     STRIPE_SECRET_KEY: 'sk_test_51JFOUzJt8PjGxb7vpU8kzIV8ayFecSJ2JmnIyMAQLknFAAVOe3V4SRHOoQIUimpdhTb7q31B5i5jkeIj8FXvPHlC00mpH0aw9E',
     STRIPE_WEBHOOK_SECRET: 'whsec_S2T6pE4bJMP62Ih6NKnDRRokCpaI5YrC',
