99b6e9690f81cfc569d566038c331a3dd5d84d36
DABAGIRE Valens <83093092+dabagirevalens@users.noreply.github.com>
2021-11-05 10:30:18 +0200

    stripe keys updated

--- next.config.js
+++ next.config.js
@@ -6,9 +6,9 @@ module.exports = {
 
     DB_URI: 'mongodb+srv://dabagire:2ussy9luzZy9Jq31@e-booking.uk9ed.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
 
-    STRIPE_API_KEY: 'pk_test_51JFOUzJt8PjGxb7vBnax4XXGVaMf9G78be5UMwj6EotdLe6mOOI6931bptYCxh0hh8ui6fsRHEXHU9u2nMNRiWEQ00GNPHuJFn',
-    STRIPE_SECRET_KEY: 'sk_test_51JFOUzJt8PjGxb7vpU8kzIV8ayFecSJ2JmnIyMAQLknFAAVOe3V4SRHOoQIUimpdhTb7q31B5i5jkeIj8FXvPHlC00mpH0aw9E',
-    STRIPE_WEBHOOK_SECRET: 'whsec_S2T6pE4bJMP62Ih6NKnDRRokCpaI5YrC',
+    STRIPE_API_KEY: 'pk_test_51JLkf4F2Ig7LH22dOF8HQLaflUYgBvmYG4DgvdhsEMMhBwtBYfpFcvCr2XpzZNUCLZMlvc6IlTMgbgKUwO4xVXus00h6S1OVmJ',
+    STRIPE_SECRET_KEY: 'sk_test_51JLkf4F2Ig7LH22dBTiOg44ELu8CzwnFVt0Pv1kFtM8mEMt010Tyk7PxQNcorFfsgL9f1ZjPi6qjnKDBP1enLDmm00GuoGMxg7',
+    STRIPE_WEBHOOK_SECRET: 'whsec_YE5qV7IRnTEzu7ztCH277wqFO9fBkOri',
     
 
     CLOUDINARY_CLOUD_NAME: "images-bucket",
